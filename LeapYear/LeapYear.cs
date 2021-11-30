ee1fdbbfa516cb9f3b745209ed596031c6a5a9d3
oizquierdo <Oizquierdo@voxelgroup.net>
2021-11-30 15:07:05 +0100

    refactor: Refactoring Calculate method

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -1,16 +1,8 @@
-﻿using System;
-
-namespace LeapYear
+﻿namespace LeapYear
 {
     public class LeapYear
     {
         public static bool Calculate(int year)
-        {
-            bool yearIsDivisibleBy4 = year % 4 == 0;
-            bool yearIsDivisibleBy100 = year % 100 == 0;
-            bool yearIsDivisibleBy400 = year % 400 == 0;
-
-            return (yearIsDivisibleBy4 && (!yearIsDivisibleBy100 || yearIsDivisibleBy400));
-        }
+            => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 }
