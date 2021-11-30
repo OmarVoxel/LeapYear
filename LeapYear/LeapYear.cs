e4715f3828ff3200f6d9a9577c190f54d77ec7c7
oizquierdo <Oizquierdo@voxelgroup.net>
2021-11-30 14:05:15 +0100

    feat: ReturnsFalseIfYearIsntDivisibleBy4

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -12,6 +12,8 @@ namespace LeapYear
                 return false;
             if (year % 4 == 0 && year % 100 != 0)
                 return true;
+            if(year % 4 != 0)
+                return false;
             return false;
         }
     }
