4ce0b11740f451249a70289431b0bf5029fb00b0
oizquierdo <Oizquierdo@voxelgroup.net>
2021-11-30 13:50:44 +0100

    test: added other case for returns true if is divisible by 400 test

--- LeapYear.Tests/YearShould.cs
+++ LeapYear.Tests/YearShould.cs
@@ -8,6 +8,7 @@ namespace LeapYear.Tests
         [Theory]
         [InlineData(2000)]
         [InlineData(4000)]
+        [InlineData(6000)]
         public void ReturnsTrueIfYearIsDivisibleBy400(int year)
         {
             Assert.True(LeapYear.Calculate(year));
