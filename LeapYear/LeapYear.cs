55fa275ef81f4c19a45b4265228d56644de23ff5
oizquierdo <Oizquierdo@voxelgroup.net>
2021-11-30 14:07:00 +0100

    refactor: if statements were refactored

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -6,13 +6,9 @@ namespace LeapYear
     {
         public static bool Calculate(int year)
         {
-            if (year % 400 == 0)
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
                 return true;
-            if (year % 400 == 0 && year % 100 != 0)
-                return false;
-            if (year % 4 == 0 && year % 100 != 0)
-                return true;
-            if(year % 4 != 0)
+            if ( year % 4 != 0 || (year % 400 == 0 && year % 100 != 0))
                 return false;
             return false;
         }
